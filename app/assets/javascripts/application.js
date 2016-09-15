8edb8fe9f9f2c229d9af9256351afed325a083b2
Jeff Hays <jhays@moguldom.com>
2016-09-15 19:59:29 -0400

    Initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,336 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require foundation
+//= require dropzone
+//= require d3
+//= require_tree .
+
+$(function() {
+  // initialize
+  $(document).foundation();
+  var chart = "#chart";
+  var table = "#chart-data";
+  var chartColors = ["#058DC7", "#50B432", "#ED561B", "#DDDF00", "#666666", "#24CBE5", "#64E572", "#FF9655", "#FFF263", "#6AF9C4", "#00AA88", "#434348", "#90ed7d", "#f7a35c", "#8085e9", "#f15c80", "#e4d354", "#8085e8", "#8d4653", "#91e8e1"];
+  var file = $(chart).data("file");
+  var hasData = false;
+
+  // draw chart if filename exists
+  if (typeof file != "undefined" && file.length) {
+    d3.json("/upload/index.json?file=" + $(chart).data("file"), function(error, data) {
+      // debugging
+      console.log(error);
+      console.log(data);
+      hasData = hasData ? hasData : !$.isEmptyObject(data);
+
+      // proceed if data isn't null
+      if (hasData) {
+
+        // create bubble chart when data is present
+        var margins = { top: 55, right: 390, bottom: 55, left: 140 },
+            width = window.innerWidth,
+            height = window.innerHeight - 40,
+            format = d3.format(",d"),
+            color = d3.scale.category20c();
+
+        // scale
+        var xScale = d3.scale.linear()
+            .domain([0, $(chart).data("packet-average")])
+            .range([0, $(chart).data("packet-average")])
+
+        // axes
+        var xAxis = d3.svg.axis()
+            .scale(xScale);
+
+        // tooltip
+        var tooltip = d3.select("body")
+            .append("div")
+            .style("position", "absolute")
+            .style("z-index", "10")
+            .style("visibility", "hidden")
+            .style("color", "white")
+            .style("padding", "8px")
+            .style("margin", "0 65px 0 0")
+            .style("background-color", "rgba(0, 0, 0, .75)")
+            .style("border-radius", "3px")
+            .style("font", "12px sans-serif")
+            .text("tooltip");
+
+        // svg
+        var svg = d3.select(chart).append("svg")
+            .on("mousemove", function() {
+              // queue tooltip over host bubble based on current mouse x position
+              var y = height / 2 - 200;
+              // cdetermine if we're on the x range of a host bubble in the front
+              var circle = xCircle(d3.event.pageX, data, width, height, margins);
+              // use the xCircle value above to 
+              var host = $("#node-" + circle + " host").text();
+              var avg = parseFloat($("#node-" + circle + " average").text());
+              var totalSize = parseInt($("#node-" + circle + " totalsize").text());
+              var maxAvg = parseInt($("#node-" + circle + " maxaverage").text());
+              var color = $("#node-" + circle + " circle").css("fill");
+              var packets = $("#node-" + circle + " packets").text();
+              var packetCount = 0;
+              if (packets.length) {
+                var packets = JSON.parse(packets);
+                if (packets.length) {
+                  packetCount = packets.length;
+                }
+              }
+              // when we match a circle to the current mouse x position
+              if (circle > -1) {
+                // loop through bubbles and queue tooltip based on current mouse x position
+                d3.select(".node").each(function(d) {
+                  // display and style the tooltip
+                  tooltip.html('<table id="d3tip"><tbody><tr><td style="text-align:right;padding-right:8px;"><div class="circle"></div>Host:</td><td>' + host + '</td></tr><tr><td style="text-align:right;padding-right:8px;"><nobr>Packet Count:</nobr></td><td>' + packetCount + '</td></tr><tr><td style="text-align:right;padding-right:8px;"><nobr>Average Size:</nobr></td><td>' + avg.toFixed(2) + '</td></tr></tbody></table><div class="button expanded" data-circle="' + circle + '">View Packets</div></div><div id="d3line"></div>');
+                  $("#d3tip .circle").css("background", color);
+                  tooltip.style({background: "rgba(0,0,0,.7)", visibility: "visible"});
+                });
+
+                // show table of packet data on tooltip button click
+                tooltip.select(".button").on("click", function() {
+                  var circle = $(this).data("circle");
+                  showTable(table, data, packets);
+                });
+
+                // show table of packet data on tooltip line click to "prevent" the line from getting in the way of the click event
+                $("#d3line").on("click", function() {
+                  var circle = $(this).data("circle");
+                  showTable(table, data, packets);
+                });
+
+                return tooltip.style("top", y + "px").style("left", (d3.event.pageX - 110) + "px");
+              }
+              return tooltip.style("visibility", "hidden");
+            })
+            .attr("width", width)
+            .attr("height", height)
+            .attr("class", "bubble");
+
+        // enter svg and create nodes
+        var node = svg.selectAll(".node")
+            .data(data)
+          .enter().append("g")
+            .attr("class", "node")
+            .attr("id", function(d) { return "node-" + d.id; })
+            .attr("transform", function(d) {
+              // calculate x position based on data and chart dimensions
+              var x = xPosition(d, width, margins);
+              return "translate(" + x  + "," + (height / 2) + ")";
+            });
+
+        // create bubble and append it to svg node
+        node.append("circle")
+            // animate in
+            .transition()
+              .duration(750)
+              .delay(function(d, i) { return i * 100; })
+            .attr("r", function(d) {
+              // calculate radius
+              var margin = margins.top + margins.bottom;
+              var circleDiameter = (d.packets.length * ((height - margin) / parseFloat(d.max)));
+              var circleRadius = circleDiameter / 2;
+              return circleRadius;
+            })
+            .style("fill", function(d) {
+              // fill color
+              var color = "hsl(" + Math.random() * 360 + ",100%,50%)";
+              if (chartColors.length) {
+                return chartColors.shift();
+              }
+              return color;
+            })
+            .each("end", function() {
+              // when the transitions have finished
+              $("#loading").fadeOut();
+            });
+
+        // add data node to easily build a table of packet data for each host bubble on click
+        node.append("packets")
+            .text(function(d) { return JSON.stringify(d.packets); });
+
+        // add id node to identify bubbles and connect the tooltip functions to each host node
+        node.append("id")
+            .text(function(d) { return d.id; });
+
+        // add x node to queue tooltip over bubble x position
+        node.append("host")
+            .text(function(d) { return d.host });
+
+        // add x node to queue tooltip over bubble x position
+        node.append("average")
+            .text(function(d) { return d.average });
+
+        // add x node to queue tooltip over bubble x position
+        node.append("totalsize")
+            .text(function(d) { return d.total_packet_size; });
+
+        // add x node to queue tooltip over bubble x position
+        node.append("maxaverage")
+            .text(function(d) { return d.max; });
+
+        // build table data on click
+        node.on("click", function(d) {
+          showTable(table, d);
+        });
+
+        d3.select(self.frameElement).style("height", width + "px");
+      } else {
+        if (error) {
+          console.log('ERROR: ', error);
+        }
+      }
+    });
+  }
+
+  // upload form
+  $("#uploadform").dropzone({
+    url: "/upload/create",
+    headers: {
+      "X-CSRFToken": $('meta[name="csrf-token"]').attr("content")
+    },
+    init: function() {
+      this.on("error", function(file, message) { alert(message); });
+    }
+  }).on("drop", function(e) {
+    processFiles(e);
+  });
+  // trigger upload form click event on icon click event
+  $("#uploadform .fa").on("click", function() {
+    $(this).parent().trigger("click");
+  });
+});
+
+$(window).scroll(function() {
+  animateRows();
+});
+
+// process files
+function processFiles(event) {
+  console.log(event);
+}
+
+// animate the table rows in to add fanciness :)
+function animateRows() {
+  // only animate the first few rows so it doesn't get annoying on larger data sets
+  var animateRows = 25;
+  $("#chart-data tbody tr:not(.animated)").each(function() {
+    if (document.body.scrollTop + window.innerHeight >= $(this).offset().top) {
+      console.log('animating in row #' + $(this).index());
+      if ($("#chart-data tbody tr").length) {
+        if ($(this).index() > animateRows) {
+          $(this).addClass("animated");
+        } else {
+          $(this).addClass("animated zoomInDown");
+        }
+      }
+    }
+  });
+}
+
+// dynamically create table of packet data
+function showTable(table, data, packets = false) {
+  var dataset = $(table).data("host");
+  // console.log( "dataset", dataset );
+  if (typeof dataset == "undefined" || dataset != data.host) {
+    if (packets) {
+      data.packets = packets;
+    }
+    // empty our table
+    $(table).empty();
+    // build our table structure
+    $(table).append(
+      $('<thead>').append(
+        $('<tr>').append(
+          $('<td>').text("Capture Length"),
+          $('<td>').text("Source IP"),
+          $('<td>').text("Desination IP"),
+          $('<td>').text("Host")
+        )
+      ),
+      $('<tbody>')
+    );
+    // build rows for each packet
+    $.each(data.packets, function(index, packet) {
+      $(table + ' tbody').append(
+        $('<tr>').append(
+          $('<td>').html(packet[0]),
+          $('<td>').html(packet[1]),
+          $('<td>').html(packet[2]),
+          $('<td>').html(packet[3])
+        )
+      );
+    });
+    // set data attribute to avoid repopulating
+    $(table).data("host", data.host);
+    // scroll to table data
+    $("body,html").animate({ scrollTop: $(table).offset().top }, 1300);
+    return true;
+  }
+  return false;
+}
+
+// get bubble data by index
+function bubbleData(index) {
+  $(".node").each(function(index, value) {
+    if (parseInt($(this).children("id").text()) == index) {
+      return { id: $(this).children('x') };
+    }
+  });
+}
+
+// position host bubbles on the x-axis
+function xPosition(data, width, margins) {
+  // set x position of node that holds each host bubble
+  var circleX = (data.average * (width / parseFloat(data.max)));
+  if (data.average > (width / 2)) {
+    var xOffset = margins.right * -1;
+  } else {
+    var xOffset = margins.left;
+  }
+  return Math.abs(circleX + xOffset);
+}
+
+// trigger tooltip on host bubble by current mouse x value
+function xCircle(x, data, width, height, margins) {
+  if (data.length) {
+    var xIn = [];
+    data.forEach(function(host, index, array) {
+      // calculate x position and left and right bounds of bubble
+      var circleX = (host.average * (width / parseFloat(host.max)));
+      // calculate x offset
+      if (host.average > (width / 2)) {
+        var xOffset = margins.right * -1;
+      } else {
+        var xOffset = margins.left;
+      }
+      // calculate new host bubble diameter based on chart size
+      var margin = margins.top + margins.bottom;
+      var circleDiameter = (host.packets.length * ((height - margin) / parseFloat(host.max)));
+      // set left and right comparison values
+      var left = (circleX + xOffset) - circleDiameter / 2;
+      var right = (circleX + xOffset) + circleDiameter / 2;
+      // create array of bubbles within bounds of the current x position
+      if (x < right && x > left) {
+        xIn.push({id: host.id, index: index});
+      }
+      // sort xIn array by index
+      xIn.sort(function(a, b) {
+        return a.index - b.index;
+      });
+    });
+    return xIn.length ? xIn.pop().id : -1;
+  }
+  return false;
+}
---
+++ app/controllers/upload_controller.rb
@@ -0,0 +1,162 @@
+class UploadController < ApplicationController
+  @@uploads = Rails.public_path + "/uploads/"
+  def create
+    redirect = "/"
+    unless params[:upload].nil?
+      # initialize
+      name = sanitize_filename(params[:upload][:file].original_filename)
+
+      # check to see if the uploads directory exist, otherwise create it
+      unless File.directory?(@@uploads)
+        Dir.mkdir @@uploads
+      end
+
+      # upload the file
+      path = File.join(@@uploads, name)
+      unless File.exists?(path)
+        File.open(path, "wb") {|f| f.write(params[:upload][:file].read)}
+      end
+
+      # redirect them to the index page and display their file
+      redirect = "/?file=" + name
+    end
+    redirect_to URI.encode(redirect)
+  end
+
+  def index
+    unless params[:file].nil?
+      # get packets from file
+      @filename = sanitize_filename(params[:file])
+      packets = get_packets(@@uploads + @filename)
+      packet_data = process_packets(packets)
+
+      # retrieve our packet data
+      @host_packets = packet_data[:packets]
+      @host_ips = packet_data[:host_ips]
+      @host_names = packet_data[:host_names]
+      @average_packet_size = packet_data[:average_packet_size]
+      @largest_total_packets = packet_data[:largest_total_packets]
+      @no_data = @host_packets.nil? && @host_packets.length
+    end
+
+    respond_to do |format|
+      format.html
+      format.json {render json: @host_packets.to_json}
+      format.xml {render xml: @host_packets.to_xml}
+    end
+  end
+
+  private
+
+  def process_packets(packets)
+    # initialize
+    new_packets = []
+    average_packet_size = []
+    host_ips = {}
+    host_names = {}
+    host_packets = {}
+    host_averages = {}
+    map = {size: 0, src: 1, dest: 2, host: 3}
+
+    # process packets
+    unless packets.nil?
+      packets.each do |packet|
+        # create hashes by host key
+        unless packet[map[:host]].nil?
+          # aggregate host ips by host
+          if host_ips[packet[map[:dest]]].nil?
+            host_ips[packet[map[:host]]] = [packet[map[:dest]]]
+          else
+            host_ips[packet[map[:host]]].push(packet[map[:dest]])
+          end
+
+          # aggregate host names by host
+          if host_names[packet[map[:dest]]].nil?
+            host_names[packet[map[:host]]] = [packet[map[:dest]]]
+          else
+            host_names[packet[map[:host]]].push(packet[map[:dest]])
+          end
+        end
+
+        # aggregate packet sizes
+        average_packet_size.push(packet[map[:size]].to_i)
+
+        # aggregate packets by source ip
+        if host_packets[packet[map[:src]]].nil?
+          # packet[map[:src]]
+          host_packets[packet[map[:src]]] = [packet]
+        else
+          host_packets[packet[map[:src]]].push(packet)
+        end
+      end
+
+      # get average packet size
+      average_packet_size = average_packet_size.sum / average_packet_size.length.to_f
+
+      # get largest total packet counts
+      largest_total_packets = 0
+      host_packets.each do |host,packets|
+        largest_total_packets = packets.length > largest_total_packets ? packets.length : largest_total_packets
+
+        # loop through packets for this host to get average packet size by host
+        average = []
+        packets.each do |packet|
+          average.push(packet[map[:size]].to_i)
+        end
+        average = average.sum / average.length.to_f
+        host_averages[host] = average
+      end
+
+      # prepare a clean data object of host packets for the view
+      index = 0
+      new_packets = []
+      host_packets.each do |host,packets|
+        # loop through packets for this host to get average packet size by host
+        average = []
+        packets.each do |packet|
+          average.push(packet[map[:size]].to_i)
+        end
+
+        # calculate data related to this host and setup a hash to deliver a nice data structure to the view
+        host = host_ips[host].nil? ? host : host_ips[host]
+        total_packet_size = average.sum
+        average = average.sum / average.length.to_f
+
+        # aggregate coordinates of outer bounds of bubble by host
+        bounds = {min: average - (packets.length / 2), max: average + (packets.length / 2)}
+
+        # push formatted packet hash
+        new_packets.push({id: index, host: host, bounds: bounds, total_packet_size: total_packet_size, max: largest_total_packets, average: average, packets: packets})
+        index += 1
+      end
+      host_packets = new_packets.sort_by {|p| p[:packets].length}.reverse
+    end
+
+    return {packets: host_packets, largest_total_packets: largest_total_packets, average_packet_size: average_packet_size, host_ips: host_ips, host_names: host_names}
+  end
+
+  def get_packets(filename)
+    # process file if one is passed
+    unless filename.nil?
+      # only continue if our sanitized filename exists in the uploads directory
+      if File.file?(filename)
+        # get the data we need from tshark while preventing tomfoolery
+        file = Shellwords.escape(filename)
+        packets = `tshark -T fields -eframe.cap_len -eip.src -eip.dst -ehttp.host -r #{filename} -Y "not icmp and (tcp.flags.syn==1 or tcp.flags.ack==1 and tcp.flags.fin==0)"`.split("\n").reject {|e| e.to_s.empty?}
+        newpackets = []
+        packets.each do |p|
+          newpackets.push(p.split("\t"))
+        end
+        packets = newpackets
+      end
+    end
+    return packets
+  end
+
+  def sanitize_filename(filename)
+    filename.strip.tap do |name|
+      name.sub! /\A.*(\\|\/)/, ''
+      name.gsub! /[^\w\.\-]/, '_'
+    end
+  end
+end
