201a2a3ec1d31483420244bed75eca4903ab26d4
Jeff Hays <jhays@moguldom.com>
2016-09-16 03:42:18 -0400

    I tried with the double quotes in my js... I really did...

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,9 +18,11 @@
 //= require_tree .
 
 $(function() {
-  // initialize foundation and d3js visualization
   $(document).foundation();
   visualize();
+  // var uploadTop = window.innerHeight - $('#uploadform').offset().top;
+
+  console.log(window.innerHeight);
 });
 
 $(window).scroll(function() {
@@ -28,17 +30,17 @@ $(window).scroll(function() {
 });
 
 // display d3js visualization
-function visualize() {
+function visualize(file = false) {
   // initialize
-  var chart = "#chart";
-  var table = "#chart-data";
-  var chartColors = ["#058DC7", "#50B432", "#ED561B", "#DDDF00", "#666666", "#24CBE5", "#64E572", "#FF9655", "#FFF263", "#6AF9C4", "#00AA88", "#434348", "#90ed7d", "#f7a35c", "#8085e9", "#f15c80", "#e4d354", "#8085e8", "#8d4653", "#91e8e1"];
-  var file = $(chart).data("file");
+  var chart = '#chart';
+  var table = '#chart-data';
+  var chartColors = ['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#666666', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4', '#00AA88', '#434348', '#90ed7d', '#f7a35c', '#8085e9', '#f15c80', '#e4d354', '#8085e8', '#8d4653', '#91e8e1'];
+  var file = file ? file : $(chart).data('file');
   var hasData = false;
 
   // draw chart if filename exists
-  if (typeof file != "undefined" && file.length) {
-    d3.json("/upload/index.json?file=" + $(chart).data("file"), function(error, data) {
+  if (typeof file != 'undefined' && file.length) {
+    d3.json('/upload/index.json?file=' + $(chart).data('file'), function(error, data) {
       // debugging
       console.log(error);
       console.log(data);
@@ -51,40 +53,43 @@ function visualize() {
         var margins = { top: 55, right: 390, bottom: 55, left: 140 },
             width = window.innerWidth,
             height = window.innerHeight - 40,
-            format = d3.format(",d"),
+            format = d3.format(',d'),
             color = d3.scale.category20c();
 
         // scale
         var xScale = d3.scale.linear()
-            .domain([0, $(chart).data("packet-average")])
-            .range([0, $(chart).data("packet-average")])
+            .domain([0, $(chart).data('packet-average')])
+            .range([0, $(chart).data('packet-average')])
 
         // axes
         var xAxis = d3.svg.axis()
             .scale(xScale);
 
         // tooltip
-        var tooltip = d3.select("body")
-            .append("div")
-            .attr("id", "d3tip")
-            .text("tooltip");
+        var tooltip = d3.select('body')
+            .append('div')
+            .attr('id', 'd3tip')
+            .text('tooltip');
 
         // svg
-        var svg = d3.select(chart).append("svg")
-            .on("mousemove", function() {
+        var svg = d3.select(chart).append('svg')
+            .on('mousemove', function() {
               // queue tooltip over host bubble based on current mouse x position
               var y = height / 2 - 200;
               // cdetermine if we're on the x range of a host bubble in the front
               var circle = xCircle(d3.event.pageX, data, width, height, margins);
               // use the xCircle value above to 
-              var host = $("#node-" + circle + " host").text();
-              host = host.indexOf(",") ? host.split(",").join("<br>") : host;
-              var avg = parseFloat($("#node-" + circle + " average").text());
-              var totalSize = parseInt($("#node-" + circle + " totalsize").text());
-              var maxAvg = parseInt($("#node-" + circle + " maxaverage").text());
-              var color = $("#node-" + circle + " circle").css("fill");
-              var packets = $("#node-" + circle + " packets").text();
+              var host = $('#node-' + circle + ' host').text();
+              // just in case there are multiple ips that resolve to different hosts, this will be a list of data instead
+              host = host.indexOf(',') ? host.split(',')[0] : host;
+              // set all the totals, averages, etc.
+              var avg = parseFloat($('#node-' + circle + ' average').text());
+              var totalSize = parseInt($('#node-' + circle + ' totalsize').text());
+              var maxAvg = parseInt($('#node-' + circle + ' maxaverage').text());
+              var color = $('#node-' + circle + ' circle').css('fill');
+              var packets = $('#node-' + circle + ' packets').text();
               var packetCount = 0;
+              // grab the packets when they're available
               if (packets.length) {
                 var packets = JSON.parse(packets);
                 if (packets.length) {
@@ -94,100 +99,100 @@ function visualize() {
               // when we match a circle to the current mouse x position
               if (circle > -1) {
                 // loop through bubbles and queue tooltip based on current mouse x position
-                d3.select(".node").each(function(d) {
+                d3.select('.node').each(function(d) {
                   // display and style the tooltip
                   tooltip.html('<table><tbody><tr><td style="text-align:right;padding-right:8px;"><div class="circle"></div>Host:</td><td>' + host + '</td></tr><tr><td style="text-align:right;padding-right:8px;"><nobr>Packet Count:</nobr></td><td>' + packetCount + '</td></tr><tr><td style="text-align:right;padding-right:8px;"><nobr>Average Size:</nobr></td><td>' + avg.toFixed(2) + '</td></tr></tbody></table><div class="button expanded" data-circle="' + circle + '">View Packets</div></div><div id="d3line"></div>');
-                  $("#d3tip table .circle").css("background", color);
-                  tooltip.style({background: "rgba(0,0,0,.7)", visibility: "visible"});
+                  $('#d3tip table .circle').css('background', color);
+                  tooltip.style({background: 'rgba(0,0,0,.7)', visibility: 'visible'});
                 });
 
                 // show table of packet data on tooltip button click
-                tooltip.select(".button").on("click", function() {
-                  var circle = $(this).data("circle");
+                tooltip.select('.button').on('click', function() {
+                  var circle = $(this).data('circle');
                   showTable(table, data, packets);
                 });
 
                 // show table of packet data on tooltip line click to "prevent" the line from getting in the way of the click event
-                $("#d3line").on("click", function() {
-                  var circle = $(this).data("circle");
+                $('#d3line').on('click', function() {
+                  var circle = $(this).data('circle');
                   showTable(table, data, packets);
                 });
-                return tooltip.style("top", y + "px").style("left", (d3.event.pageX - 110) + "px");
+                return tooltip.style('top', y + 'px').style('left', (d3.event.pageX - 110) + 'px');
               }
-              return tooltip.style("visibility", "hidden");
+              return tooltip.style('visibility', 'hidden');
             })
-            .attr("width", width)
-            .attr("height", height)
-            .attr("class", "bubble");
+            .attr('width', width)
+            .attr('height', height)
+            .attr('class', 'bubble');
 
         // enter svg and create nodes
-        var node = svg.selectAll(".node")
+        var node = svg.selectAll('.node')
             .data(data)
-          .enter().append("g")
-            .attr("class", "node")
-            .attr("id", function(d) { return "node-" + d.id; })
-            .attr("transform", function(d) {
+          .enter().append('g')
+            .attr('class', 'node')
+            .attr('id', function(d) { return 'node-' + d.id; })
+            .attr('transform', function(d) {
               // calculate x position based on data and chart dimensions
               var x = xPosition(d, width, margins);
-              return "translate(" + x  + "," + (height / 2) + ")";
+              return 'translate(' + x  + ',' + (height / 2) + ')';
             });
 
         // create bubble and append it to svg node
-        node.append("circle")
+        node.append('circle')
             // animate in
             .transition()
               .duration(750)
               .delay(function(d, i) { return i * 100; })
-            .attr("r", function(d) {
+            .attr('r', function(d) {
               // calculate radius
               var margin = margins.top + margins.bottom;
               var circleDiameter = (d.packets.length * ((height - margin) / parseFloat(d.max)));
               var circleRadius = circleDiameter / 2;
               return circleRadius;
             })
-            .style("fill", function(d) {
+            .style('fill', function(d) {
               // fill color
-              var color = "hsl(" + Math.random() * 360 + ",100%,50%)";
+              var color = 'hsl(' + Math.random() * 360 + ',100%,50%)';
               if (chartColors.length) {
                 return chartColors.shift();
               }
               return color;
             })
-            .each("end", function() {
+            .each('end', function() {
               // when the transitions have finished
-              $("#loading").fadeOut();
+              $('#loading').fadeOut();
             });
 
         // add data node to easily build a table of packet data for each host bubble on click
-        node.append("packets")
+        node.append('packets')
             .text(function(d) { return JSON.stringify(d.packets); });
 
         // add id node to identify bubbles and connect the tooltip functions to each host node
-        node.append("id")
+        node.append('id')
             .text(function(d) { return d.id; });
 
         // add x node to queue tooltip over bubble x position
-        node.append("host")
+        node.append('host')
             .text(function(d) { return d.host });
 
         // add x node to queue tooltip over bubble x position
-        node.append("average")
+        node.append('average')
             .text(function(d) { return d.average });
 
         // add x node to queue tooltip over bubble x position
-        node.append("totalsize")
+        node.append('totalsize')
             .text(function(d) { return d.total_packet_size; });
 
         // add x node to queue tooltip over bubble x position
-        node.append("maxaverage")
+        node.append('maxaverage')
             .text(function(d) { return d.max; });
 
         // build table data on click
-        node.on("click", function(d) {
+        node.on('click', function(d) {
           showTable(table, d);
         });
 
-        d3.select(self.frameElement).style("height", width + "px");
+        d3.select(self.frameElement).style('height', width + 'px');
       } else {
         if (error) {
           console.log('ERROR: ', error);
@@ -197,32 +202,36 @@ function visualize() {
   }
 
   // upload form
-  $("#uploadform").dropzone({
-    url: "/upload/create",
+  $('#uploadform').dropzone({
+    url: '/upload/create',
     headers: {
-      "X-CSRFToken": $('meta[name="csrf-token"]').attr("content")
+      'X-CSRFToken': $('meta[name="csrf-token"]').attr("content")
     },
     params: {
         _token: $('meta[name="csrf-token"]').attr("content")
     },
     init: function() {
-      this.on("addedfile", function(file) {
-        console.log("adding file " + file);
+      this.on('addedfile', function(file) {
+        console.log('adding file ' + file);
       }),
-      this.on("success", function(file, response) {
+      this.on('success', function(file, response) {
           console.log(response);
+          // animate out loading icon and animate in response message
+          // load d3js visualization from file in ajax response
       })
-      console.log('init dropzone');
     },
     drop: function() {
+      $('<')
+      $('.fa-cloud-upload').hide();
       console.log('drop');
     }
-  }).on("drop", function(e) {
-    processFiles(e);
+  }).on('drop', function(e) {
+    // dropped file
+    console.log('dropped file');
   });
   // trigger upload form click event on icon click event
-  $("#uploadform .fa").on("click", function() {
-    $(this).parent().trigger("click");
+  $('#uploadform .fa').on('click', function() {
+    $(this).parent().trigger('click');
   });
 }
 
@@ -230,13 +239,13 @@ function visualize() {
 function animateRows() {
   // only animate the first few rows so it doesn't get annoying on larger data sets
   var animateRows = 25;
-  $("#chart-data tbody tr:not(.animated)").each(function() {
+  $('#chart-data tbody tr:not(.animated)').each(function() {
     if (document.body.scrollTop + window.innerHeight >= $(this).offset().top) {
-      if ($("#chart-data tbody tr").length) {
+      if ($('#chart-data tbody tr').length) {
         if ($(this).index() > animateRows) {
-          $(this).addClass("animated");
+          $(this).addClass('animated');
         } else {
-          $(this).addClass("animated zoomInDown");
+          $(this).addClass('animated zoomInDown');
         }
       }
     }
@@ -245,8 +254,8 @@ function animateRows() {
 
 // dynamically create table of packet data
 function showTable(table, data, packets = false) {
-  var dataset = $(table).data("host");
-  if (typeof dataset == "undefined" || dataset != data.host) {
+  var dataset = $(table).data('host');
+  if (typeof dataset == 'undefined' || dataset != data.host) {
     if (packets) {
       data.packets = packets;
     }
@@ -256,10 +265,10 @@ function showTable(table, data, packets = false) {
     $(table).append(
       $('<thead>').append(
         $('<tr>').append(
-          $('<td>').text("Capture Length"),
-          $('<td>').text("Source IP"),
-          $('<td>').text("Desination IP"),
-          $('<td>').text("Host")
+          $('<td>').text('Capture Length'),
+          $('<td>').text('Source IP'),
+          $('<td>').text('Desination IP'),
+          $('<td>').text('Host')
         )
       ),
       $('<tbody>')
@@ -276,9 +285,9 @@ function showTable(table, data, packets = false) {
       );
     });
     // set data attribute to avoid repopulating
-    $(table).data("host", data.host);
+    $(table).data('host', data.host);
     // scroll to table data
-    $("body,html").animate({ scrollTop: $(table).offset().top }, 1300);
+    $('body,html').animate({ scrollTop: $(table).offset().top }, 1300);
     return true;
   }
   return false;
